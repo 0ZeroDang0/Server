ef26c8663b565b94b7e13e531ef1941a182a27d9
김멍몽 <wjdahrrla@gmail.com>
2024-07-27 00:44:06 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/example/zerodang/ZeroDangApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerodang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroDangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zerodang/ZeroDangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerodang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroDangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
