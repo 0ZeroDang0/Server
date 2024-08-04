f1c50c84bacd7aec8aa8283c66d68d9ba1d1a93c
김멍몽 <wjdahrrla@gmail.com>
2024-08-05 00:17:48 +0900

    :

--- src/main/java/com/example/zerodang/global/config/RedisConfig.java
+++ src/main/java/com/example/zerodang/global/config/RedisConfig.java
@@ -1,15 +1,26 @@
 package com.example.zerodang.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 // docker run -d -p 6379:6379 redis
 @Configuration
 public class RedisConfig {
+    @Value("${jwt.redis.host}")
+    private String host;
 
+    @Value("${jwt.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
     @Bean
     public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
