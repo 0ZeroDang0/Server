af0b5240bf03976cc67e1e2b1da838ac53f09db5
김멍몽 <wjdahrrla@gmail.com>
2024-08-06 23:21:17 +0900

    fix: prompt 수정

--- src/main/java/com/example/zerodang/global/gpt/service/GptServiceImpl.java
+++ src/main/java/com/example/zerodang/global/gpt/service/GptServiceImpl.java
@@ -38,14 +38,14 @@ public class GptServiceImpl implements GptService{
         String productDescription2 = product2.getProductDescription();
 
         String prompt = String.format(
-                "이름 : %s\n원재료 : %s\n\n이름 : %s\n원재료 : %s\n\n두 제품에 들어간 감미료들을 찾아주고 두 제품에 들어간 감미료를 보면서 어떤 걸 섭취하는 게 더 나을지 분석해줘.\n" +
+                "이름 : %s\n원재료 : %s\n\n이름 : %s\n원재료 : %s\n\n두 제품에 들어간 감미료들을 찾아주고 두 제품에 들어간 감미료를 보면서 어떤 걸 섭취하는 게 더 나을지 분석해줘. 말투는 '~ 입니다'로 해줘.\n" +
                         "응답을 아래와 같은 형식으로 주세요:\n\n" +
                         "감미료 분석:\n" +
                         "비교 및 결론:\n" +
                         "안전성: 설명\n" +
                         "칼로리: 설명\n" +
                         "부작용: 설명\n" +
-                        "따라서, 결론"
+                        "결론 : 설명"
                 , productName1, productDescription1, productName2, productDescription2
         );
 
